cb8678c6ec46ac44b10fd6cf2c63039509b36057
Miguel Fernandes <miguelmanofernandes@gmail.com>
2020-12-08 15:30:45 +0000

    Add INSTR structs and OPCODE conversion map

--- d8-handheld-halting/main.cpp
+++ d8-handheld-halting/main.cpp
@@ -1,12 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef pair<string,int> Instr;
+enum OPCODE {ACC, JMP, NOP};
+
+static map<string, OPCODE> opcode_table = {
+    { "acc", OPCODE::ACC },
+    { "jmp", OPCODE::JMP },
+    { "nop", OPCODE::NOP }
+};
+
+struct INSTR {
+    OPCODE opcode;
+    int value;
+    
+    INSTR(string name, int value) {
+        this->opcode = opcode_table[name];
+        this->value = value;
+    }
+};
 
 struct Program {
     int acc = 0;
     
-    bool run(vector<Instr> instrs) {
+    bool run(vector<INSTR> instrs) {
         set<int> idx;
 
         for (int i = 0; i < instrs.size(); i++) {
@@ -15,44 +31,56 @@ struct Program {
 
             idx.insert(i);
 
-            if (instrs[i].first == "acc")
-                acc += instrs[i].second;
+            switch (instrs[i].opcode) {
+                case OPCODE::ACC:
+                    acc += instrs[i].value;
+                    break;
 
-            if (instrs[i].first == "jmp")
-                i += instrs[i].second - 1;
+                case OPCODE::JMP:
+                    i += instrs[i].value - 1;
+                    break;
+                
+                default:
+                    break;
+            }  
         }
         return true;
     }
 };
 
-int p2(vector<Instr> instrs) {
-    vector<Instr> copy = instrs;
+int p2(vector<INSTR> instrs) {
+    vector<INSTR> copy = instrs;
 
     for (int i = 0; i < instrs.size(); i++) {
         Program p;
 
-        if (instrs[i].first == "nop") {
-            instrs[i].first = "jmp";
-            if (p.run(instrs)) return p.acc;
-        }
-        
-        else if (instrs[i].first == "jmp") {
-            instrs[i].first = "nop";
-            if (p.run(instrs)) return p.acc;
+        switch (instrs[i].opcode) {
+            case OPCODE::NOP:
+                instrs[i].opcode = OPCODE::JMP;
+                if (p.run(instrs)) return p.acc;
+                break;
+
+            case OPCODE::JMP:
+                instrs[i].opcode = OPCODE::NOP;
+                if (p.run(instrs)) return p.acc;
+                break;
+            
+            default:
+                break;
         }
         instrs = copy;
     }
-    return -1;
+    return INT_MAX;
 }
 
-int p1(vector<Instr> instrs) {
+int p1(vector<INSTR> instrs) {
     Program p;
     p.run(instrs);
     return p.acc;
 }
 
 int main() {
-    vector<Instr> instrs;
+    vector<INSTR> instrs;
 
     for (string line; getline(cin, line);) {
         stringstream ss(line);
@@ -61,7 +89,7 @@ int main() {
         for (string token; getline(ss, token, ' ');)
             tokens.push_back(token);
 
-        instrs.push_back({tokens[0], stoi(tokens[1])});
+        instrs.push_back(INSTR(tokens[0], stoi(tokens[1])));
     }
 
     cout << p1(instrs) << endl;
