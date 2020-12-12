d95d5e7ebe2dc7d043af2c5a8de8969ec2907bcd
Miguel Fernandes <miguelmanofernandes@gmail.com>
2020-12-12 04:14:27 +0000

    Add solution for day #11

--- d11-seating-system/main.cpp
+++ d11-seating-system/main.cpp
@@ -3,20 +3,69 @@ using namespace std;
 
 typedef vector<vector<char>> Ferry;
 
-int p1() {
+Ferry initial_ferry;
+vector<pair<int,int>> adjacency = {
+    {-1, 0}, {-1, 1}, {0, 1}, {1, 1},
+    {1, 0}, {1, -1}, {0, -1}, {-1, -1}
+};
 
+int count_occupied(Ferry &ferry) {
+    int count = 0;
+    for (auto row: ferry) for (char c: row) if (c == '#') count++;
+    return count;
+}
+
+bool is_valid(int i, int j) {
+    return i >= 0 && j >= 0 && i < initial_ferry.size() && j < initial_ferry[i].size();
+}
+
+pair<int,int> get_immediate(int i, int j, pair<int,int> k) {
+    return {i + k.first, j + k.second};
+}
+
+pair<int,int> get_visible(int i, int j, pair<int,int> k) {
+    do { i += k.first, j += k.second; } 
+    while (is_valid(i, j) && initial_ferry[i][j] == '.');
+    return {i, j};
+}
+
+int run(pair<int,int> strategy(int i, int j, pair<int,int> k), int rule) {
+    Ferry ferry = initial_ferry;
+    bool has_changed = false;
+
+    do {
+        Ferry temp_ferry = ferry;
+        has_changed = false;
+
+        for (int i = 0; i < ferry.size(); i++) {
+            for (int j = 0; j < ferry[i].size(); j++) {
+
+                int adjs = count_if(adjacency.begin(), adjacency.end(), [=](const pair<int,int> k) {
+                    pair<int,int> adj = strategy(i, j, k);
+                    return is_valid(adj.first, adj.second) && ferry[adj.first][adj.second] == '#';
+                });
+
+                if (ferry[i][j] == 'L' && adjs == 0) {
+                    temp_ferry[i][j] = '#';
+                    has_changed = true;
+                }
+
+                else if (ferry[i][j] == '#' && adjs >= rule) {
+                    temp_ferry[i][j] = 'L';
+                    has_changed = true;
+                }
+            }
+        }
+        ferry = temp_ferry;
+
+    } while (has_changed);
+
+    return count_occupied(ferry);
 }
 
 int main() {
-    Ferry ferry;
-
     for (string line; getline(cin, line);)
-        ferry.push_back(vector<char>(line.begin(), line.end()));
+        initial_ferry.push_back(vector<char>(line.begin(), line.end()));
 
-    for (int i = 0; i < ferry.size(); i++) {
-        for (int j = 0; j < ferry[i].size(); j++) {
-            cout << ferry[i][j] << " ";
-        }
-        cout << endl;
-    }
+    printf("p1: %d\np2: %d\n", run(get_immediate, 4), run(get_visible, 5));
 }
