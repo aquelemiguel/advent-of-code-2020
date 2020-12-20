7ad6f08a8f914b865e6bb797ee9f15c669ef361b
Miguel Fernandes <miguelmanofernandes@gmail.com>
2020-12-20 05:25:11 +0000

    Cleanup day #20 solution

--- 18-operation-order/main.cpp
+++ 18-operation-order/main.cpp
@@ -4,170 +4,83 @@ using namespace std;
 
 typedef unsigned long long ull;
 
+ull calc_RPN(queue<char> q) {
+    stack<ull> res;
+
+    while (!q.empty()) {
+        if (isdigit(q.front())) res.push(q.front() - '0');
+        else {
+            ull a = res.top(); res.pop();
+            ull b = res.top(); res.pop();
+            res.push(q.front() == '+' ? a+b : a*b);
+        }
+        q.pop();
+    }
+    return res.top();
+}
+
 ull shunting_yard_v2(vector<char> tokens) {
     queue<char> q;
     stack<char> s;
 
-    size_t i = 0;
-
-    while (i < tokens.size()) {
-        // cout << tokens[i] << endl;
-        
-        if (isdigit(tokens[i])) {
-            // cout << "pushed to queue: " << tokens[i] << endl;
+    for (size_t i = 0; i < tokens.size(); i++) {
+        if (isdigit(tokens[i]))
             q.push(tokens[i]);
-        }
 
-        else if (tokens[i] == '(' || tokens[i] == '+') {
-            // cout << "pushed to stack: " << tokens[i] << endl;
+        else if (tokens[i] == '(' || tokens[i] == '+')
             s.push(tokens[i]);
-        }
 
         else if (tokens[i] == '*') {
-            // cout << "pushed to stack: " << tokens[i] << endl;
-
             while (!s.empty() && s.top() == '+') {
-                q.push(s.top());
-                s.pop();
+                q.push(s.top()); s.pop();
             }
-
             s.push(tokens[i]);
         }
 
         else if (tokens[i] == ')') {
             while (s.top() != '(') {
-                // cout << "pushed to queue: " << tokens[i] << endl;
-                q.push(s.top());
-                s.pop();
+                q.push(s.top()); s.pop();
             }
             s.pop();
         }
-        i++;
     }
 
     while (!s.empty()) {
-        q.push(s.top());
-        s.pop();
+        q.push(s.top()); s.pop();
     }
-
-    
-    // while (!q.empty()) {
-    //     cout << q.front() << " ";
-    //     q.pop();
-    // }
-    // cout << endl;
-
-    stack<ull> res;
-
-    while (!q.empty()) {
-        // cout << q.front() << endl;
-
-        if (isdigit(q.front())) {
-            res.push(q.front() - '0');
-            // cout << "converted: " << q.front() - '0' << endl;
-        } 
-        else {
-            ull a = res.top();
-            res.pop();
-            ull b = res.top();
-            res.pop();
-            
-            if (q.front() == '+') {
-                res.push(a + b);
-                // cout << "pushed to res: " << a << "+" << b << "=" << a+b << endl;
-            }
-            else {
-                res.push(a * b);
-                // cout << "pushed to res: " << a << "*" << b << "=" << a*b << endl;
-            }
-        }
-        q.pop();
-    }
-
-    return res.top();
+    return calc_RPN(q);
 }
 
 ull shunting_yard_v1(vector<char> tokens) {
     queue<char> q;
     stack<char> s;
 
-    size_t i = 0;
-
-    while (i < tokens.size()) {
-        // cout << tokens[i] << endl;
-        
-        if (isdigit(tokens[i])) {
-            // cout << "pushed to queue: " << tokens[i] << endl;
+    for (size_t i = 0; i < tokens.size(); i++) {
+        if (isdigit(tokens[i]))
             q.push(tokens[i]);
-        }
 
-        else if (tokens[i] == '(') {
-            // cout << "pushed to stack: " << tokens[i] << endl;
+        else if (tokens[i] == '(')
             s.push(tokens[i]);
-        }
 
         else if (tokens[i] == '+' || tokens[i] == '*') {
-            // cout << "pushed to stack: " << tokens[i] << endl;
-
             while (!s.empty() && (s.top() == '+' || s.top() == '*')) {
-                q.push(s.top());
-                s.pop();
+                q.push(s.top()); s.pop();
             }
-
             s.push(tokens[i]);
         }
 
         else if (tokens[i] == ')') {
             while (s.top() != '(') {
-                // cout << "pushed to queue: " << tokens[i] << endl;
-                q.push(s.top());
-                s.pop();
+                q.push(s.top()); s.pop();
             }
             s.pop();
         }
-        i++;
     }
 
     while (!s.empty()) {
-        q.push(s.top());
-        s.pop();
+        q.push(s.top()); s.pop();
     }
-
-    
-    // while (!q.empty()) {
-    //     cout << q.front() << " ";
-    //     q.pop();
-    // }
-    // cout << endl;
-
-    stack<ull> res;
-
-    while (!q.empty()) {
-        // cout << q.front() << endl;
-
-        if (isdigit(q.front())) {
-            res.push(q.front() - '0');
-            // cout << "converted: " << q.front() - '0' << endl;
-        } 
-        else {
-            ull a = res.top();
-            res.pop();
-            ull b = res.top();
-            res.pop();
-            
-            if (q.front() == '+') {
-                res.push(a + b);
-                // cout << "pushed to res: " << a << "+" << b << "=" << a+b << endl;
-            }
-            else {
-                res.push(a * b);
-                // cout << "pushed to res: " << a << "*" << b << "=" << a*b << endl;
-            }
-        }
-        q.pop();
-    }
-
-    return res.top();
+    return calc_RPN(q);
 }
 
 int main() {
